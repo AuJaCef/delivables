4a82c93bd71df0c4b1890b1e405698513e6bc839
AuJaCef <45008462+AuJaCef@users.noreply.github.com>
2018-11-14 13:48:12 -0500

    Add files via upload

---
+++ challenge_1.2/challenge.py
@@ -0,0 +1,8 @@
+#challenge
+#Austin C 11/14/18
+#for the challenge
+
+def main():
+    for i in ["cake", "pie", "ice cream", "candy"]:
+        print (i)
+main()
---
+++ challenge_1.2/name_greet.py
@@ -0,0 +1,15 @@
+#name reader
+#Austin C 11/14/18
+#remembers name
+
+def main() :
+    name = 5
+    
+    first_name = eval(input("What is your first name: "))
+    last_name = eval(input("What is your last name: "))
+
+    name = "first_name" + "last_name"
+
+    print("Hello, ", name)
+
+main()
